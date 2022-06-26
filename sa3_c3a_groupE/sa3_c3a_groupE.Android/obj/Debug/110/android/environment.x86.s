c044fa974e650ee5071d0db70789dac6338f9e4c
zrta2480 <96914273+zrta2480@users.noreply.github.com>
2022-06-26 21:09:21 +0800

    Updated mainpage and display page UI
    Added a background image in Main page, set title bar of main page to hidden, added a title to title bar in display page

--- sa3_c3a_groupE/sa3_c3a_groupE.Android/obj/Debug/110/android/environment.x86.s
+++ sa3_c3a_groupE/sa3_c3a_groupE.Android/obj/Debug/110/android/environment.x86.s
@@ -71,7 +71,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"b57b97ea-3917-4d28-834f-3a990e7dfe34"
+	.asciz	"e9537fe0-7196-45c9-b55c-844b8ff07976"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
